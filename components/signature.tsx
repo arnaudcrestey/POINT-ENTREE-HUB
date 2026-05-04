5a1977fc7919f82d219115b0f8eb819696b1ea4a
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-05-04 23:02:49 +0200

    Update signature.tsx

--- components/signature.tsx
+++ components/signature.tsx
@@ -1,7 +1,7 @@
 export function Signature() {
   return (
     <p className="text-xs tracking-[0.18em] text-black/45 uppercase">
-      Arnaud Crestey · Orientation stratégique
+      Orientation stratégique
     </p>
   );
 }
