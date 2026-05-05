7a0b7798a7fda55bcba1cdb79b60716d92c01bd7
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-05-05 14:19:30 +0200

    Update signature.tsx

--- components/signature.tsx
+++ components/signature.tsx
@@ -1,7 +1,7 @@
 export function Signature() {
   return (
     <p className="text-xs tracking-[0.18em] text-black/45 uppercase">
-      Positionnement
+      Orientation Professionnelle
     </p>
   );
 }
