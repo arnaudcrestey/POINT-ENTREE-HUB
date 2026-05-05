1c789e2a0be74e87df830f97566df5b76f612f44
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-05-05 18:11:15 +0200

    Update questions.ts

--- lib/questions.ts
+++ lib/questions.ts
@@ -28,7 +28,7 @@ export const primaryOptions: PrimaryOption[] = [
       },
       {
         id: "s2",
-        prompt: "On vous confie un projet flou. Dans l’heure, vous",
+        prompt: "On vous confie un projet flou. Dans l’heure, vous :",
         answers: [
           { id: "s2a", label: "Définissez un cadre, des étapes et un plan d’action", delta: { structuration: 3 } },
           { id: "s2b", label: "Posez des questions pour comprendre", delta: { comprehension: 2 } },
@@ -278,18 +278,18 @@ export const primaryOptions: PrimaryOption[] = [
           { id: "v7c", label: "Comprendre", delta: { comprehension: 2 } }
         ]
       },
-  {
-  id: "v8",
-  prompt: "Quand un rendu manque d’impact, votre premier réflexe est",
-  answers: [
-    { id: "v8a", label: "Repenser immédiatement la perception pour le rendre plus désirable", delta: { valorisation: 3 } },
-    { id: "v8b", label: "Structurer le contenu pour le rendre plus clair", delta: { structuration: 2 } },
-    { id: "v8c", label: "Analyser ce qui ne fonctionne pas", delta: { comprehension: 2 } }
-       ]
+      {
+        id: "v8",
+        prompt: "Quand un rendu manque d’impact, votre premier réflexe est :",
+        answers: [
+          { id: "v8a", label: "Repenser immédiatement la perception pour le rendre plus désirable", delta: { valorisation: 3 } },
+          { id: "v8b", label: "Structurer le contenu pour le rendre plus clair", delta: { structuration: 2 } },
+          { id: "v8c", label: "Analyser ce qui ne fonctionne pas", delta: { comprehension: 2 } }
+        ]
       },
       {
         id: "v9",
-        prompt: "Face un produit ou une offre :",
+        prompt: "Face à un produit ou une offre :",
         answers: [
           { id: "v9a", label: "Vous voyez son potentiel visuel immédiatement", delta: { valorisation: 3 } },
           { id: "v9b", label: "Vous analysez", delta: { comprehension: 2 } },
