d807b8b00e3fb477ba1da4c3b656732c8ec55ae3
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-05-06 13:07:59 +0200

    Update signature.tsx

--- components/signature.tsx
+++ components/signature.tsx
@@ -5,19 +5,40 @@ export function Signature() {
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Ouvrir arnaudcrestey.com"
-      className="group inline-flex w-fit flex-col items-center text-center no-underline"
+      className="inline-flex w-fit flex-col items-center text-center no-underline"
+      style={{ paddingBottom: "14px" }}
     >
-      <span className="block font-serif text-[64px] leading-[0.85] tracking-[-0.16em] text-black">
+      <span
+        className="font-serif text-black"
+        style={{
+          fontSize: "64px",
+          lineHeight: "0.86",
+          letterSpacing: "-0.16em",
+        }}
+      >
         AC
       </span>
 
-      <span className="mt-3 block font-serif text-[18px] leading-none text-black">
+      <span
+        className="font-serif text-black"
+        style={{
+          marginTop: "12px",
+          fontSize: "18px",
+          lineHeight: "1",
+        }}
+      >
         arnaudcrestey.com
       </span>
 
       <span
         aria-hidden="true"
-        className="mt-4 block h-[1px] w-[72px] bg-[#c8a46b]"
+        style={{
+          display: "block",
+          width: "72px",
+          height: "1px",
+          marginTop: "16px",
+          backgroundColor: "#c8a46b",
+        }}
       />
     </a>
   );
